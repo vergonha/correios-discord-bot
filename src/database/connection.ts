8f53ee510b01101c9988d964a8d332dbb56367da
vergonha <shirosayuya@gmail.com>
2026-05-13 10:16:30 -0300

    🔨 Atualizando dependências

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,21 +1,30 @@
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import logger from '../logger.js'
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import logger from "../logger.js";
+import { setServers } from "node:dns/promises";
 
-dotenv.config()
+setServers(["1.1.1.1", "8.8.8.8"]);
+dotenv.config();
 
-const connectionString = process.env.MONGODB_CONNECTION_STRING
-export default function connection(){
-    if(!connectionString){
-        logger.error("🍂 Verifique se o seu arquivo .ENV está preenchido corretamente.")
-        process.exit()
-    }
+const connectionString = process.env.MONGODB_CONNECTION_STRING;
+export default function connection() {
+  if (!connectionString) {
+    logger.error(
+      "🍂 Verifique se o seu arquivo .ENV está preenchido corretamente.",
+    );
+    process.exit();
+  }
 
-    mongoose.connect(connectionString)
-        .then(_ => {logger.info("🌿 Conexão com a database estabelecida.")})
-        .catch(err => {
-            logger.error("🍂 Erro ao estabelecer conexão com a database | Verifique se a String de conexão com o MongoDB é válida.")
-            logger.error(err.message)
-            process.exit()
-        })
+  mongoose
+    .connect(connectionString)
+    .then((_) => {
+      logger.info("🌿 Conexão com a database estabelecida.");
+    })
+    .catch((err) => {
+      logger.error(
+        "🍂 Erro ao estabelecer conexão com a database | Verifique se a String de conexão com o MongoDB é válida.",
+      );
+      logger.error(err.message);
+      process.exit();
+    });
 }
